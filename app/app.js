1eb891547735e7c8e4568b7e2c6b56dd910b6159
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2016-09-30 17:29:58 -0400

    Added starter javaascript code, get the days for the rest of the week, and have a workday constructor.

---
+++ app/app.js
@@ -0,0 +1,38 @@
+function Workday() {
+  day = '';
+  lunch = '';
+  dinner = '';
+}
+
+var menu = {
+  init: function(){
+    menu.getDay();
+    this.setupEventListeners();
+  },
+
+  setupEventListeners: function(){
+
+  },
+
+  getDay: function(){
+    var date = new Date();
+    var day = date.getDay() -1;
+    if(day == -1){
+      day = 6;
+    }
+    var arr = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+    while(day <= 6){
+      menu.buildDay(arr[day]);
+      day++
+    }
+  },
+
+  buildDay: function(currday){
+    var day = new Workday();
+    day.day = currday;
+    console.log(day);
+  }
+
+}
+menu.init();
