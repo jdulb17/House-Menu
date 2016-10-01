8229387f1fccefc27ca352d2326692ce716687d0
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2016-09-30 22:42:41 -0400

    refactored code from home to form controller.

--- app/form/form.controller.js
+++ app/form/form.controller.js
@@ -7,7 +7,32 @@
 
     FormController.$inject = [];
     function FormController() {
+      var vm = this;
 
+      vm.Workday = Workday;
+      vm.getDay = getDay()
+
+
+
+      function Workday() {
+        day = '';
+        lunch = '';
+        dinner = '';
+      }
+
+      function getDay() {
+          var date = new Date();
+          var day = date.getDay() - 1;
+          if (day == -1) {
+              day = 6;
+          }
+          var arr = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+          while (day <= 6) {
+              console.log(arr[day]);
+              day++
+          }
+      }
     }
-    
+
 })();
