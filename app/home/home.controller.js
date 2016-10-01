dea88ecc237c8489a18d740108d85410ac31eeae
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2016-09-30 22:38:06 -0400

    Added a form folder with a controller, and route-config file. I also linked it to the home screen.

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -7,7 +7,33 @@
 
     HomeController.$inject = [];
     function HomeController() {
+      var vm = this;
+
+      vm.Workday = Workday;
+      vm.getDay = getDay()
+
+
+
+      function Workday() {
+        day = '';
+        lunch = '';
+        dinner = '';
+      }
+
+      function getDay() {
+          var date = new Date();
+          var day = date.getDay() - 1;
+          if (day == -1) {
+              day = 6;
+          }
+          var arr = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+          while (day <= 6) {
+              console.log(arr[day]);
+              day++
+          }
+      }
 
     }
-    
+
 })();
